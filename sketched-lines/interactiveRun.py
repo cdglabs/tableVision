6ffd5a6ae7c9ae6cf2143d82aed39881729be728
matmax <matthias.graf@st.ovgu.de>
2015-10-20 13:02:15 -0700

    tethered capture on linux, white balance working & improvements to interactiveRun wrapper

--- sketched-lines/interactiveRun.py
+++ sketched-lines/interactiveRun.py
@@ -1,30 +1,53 @@
 import cv2
 import numpy as np
 import sys
+import subprocess
 
-assert len(sys.argv) > 1, "first command line argument must be module name to run"
-print "running: "+sys.argv[1]
-moduleToRun = __import__(sys.argv[1])
+useLiveVideo=True
+runInteractive=True
+moduleToRun=None
 methodNameToRun = "withImage"
-assert hasattr(moduleToRun, methodNameToRun), "module has to have "+methodNameToRun+" method"
+frame=None
+capture=None
 
-cap = cv2.VideoCapture(1)
-# resolution
-cap.set(3, 1920)
-cap.set(4, 1080)
+def getFreeFilename():
+	filenameNumber = 1
+	getFilename = lambda: os.getcwd()+"/"+"photo"+str(filenameNumber).zfill(3)+".jpg"
+	while os.path.isfile(getFilename()):
+		filenameNumber += 1
+	return getFilename()
 
-moduleToRun = reload(moduleToRun)
+def takeNewPhotoFromCamera():
+	file=getFreeFilename()
+	print "trying to capture: "+file
+	bashCommand = "gphoto2 --capture-image-and-download --filename="+file
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	bashOut = process.communicate()[0]
+	print bashOut
+	assert os.path.isfile(file)
+	return file
 
-while(True):
+def setUpModuleToBeRun():
+	assert len(sys.argv) > 1, "first command line argument must be module name to run"
+	print "running: "+sys.argv[1]
+	moduleToRun = __import__(sys.argv[1])
+	assert hasattr(moduleToRun, methodNameToRun), "module has to have "+methodNameToRun+" method"
+	return moduleToRun
+
+def runOnce(moduleToRun):
+	global frame, methodNameToRun, capture
 	try:
 		moduleToRun = reload(moduleToRun)
-		ret, frame = cap.read()
+		if useLiveVideo:
+			ret, frame = capture.read()
 		result = getattr(moduleToRun, methodNameToRun)(frame)
 		if isinstance(result, list):
 			for i in range(len(result)):
 				windowName = 'frame'+str(i)
-				cv2.imshow(windowName, result[i])
+				cv2.namedWindow(windowName, cv2.WINDOW_NORMAL)
+				#cv2.resizeWindow(windowName, 900, 600)
 				#cv2.moveWindow(windowName, 100, 100)
+				cv2.imshow(windowName, result[i])
 		else:
 			cv2.imshow('frame', result)
 	except:
@@ -32,7 +55,32 @@ while(True):
 		raise
 	
 	if cv2.waitKey(1) & 0xFF == ord('q'):
-		break
+		return True #break
 
-cap.release()
-cv2.destroyAllWindows()
+def main():
+	global frame, capture
+	moduleToRun = setUpModuleToBeRun()
+	
+	if useLiveVideo:
+		capture = cv2.VideoCapture(0)
+		# resolution
+		capture.set(3, 1920)
+		capture.set(4, 1080)
+	else:
+		frame = cv2.imread("photo001.jpg")
+	
+	if runInteractive:
+		moduleToRun = reload(moduleToRun)
+		while(True):
+			if runOnce(moduleToRun) == True:
+				break
+	else:
+		runOnce(moduleToRun)
+	
+	if useLiveVideo:
+		cap.release()
+	else:
+		cv2.waitKey(0)
+	cv2.destroyAllWindows()
+
+main()
---
+++ sketched-lines/takePictureFromCamera.py
@@ -0,0 +1,32 @@
+import numpy as np
+import cv2
+import os.path
+import subprocess
+
+def getFreeFilename():
+	filenameNumber = 1
+	getFilename = lambda: os.getcwd()+"/"+"photo"+str(filenameNumber).zfill(3)+".jpg"
+	while os.path.isfile(getFilename()):
+		filenameNumber += 1
+	return getFilename()
+
+def takeNewPhotoFromCamera():
+	file=getFreeFilename()
+	print "trying to capture: "+file
+	bashCommand = "gphoto2 --capture-image-and-download --filename="+file
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	bashOut = process.communicate()[0]
+	print bashOut
+	assert os.path.isfile(file)
+	return file
+
+#img = cv2.imread(takeNewPhotoFromCamera())
+img = cv2.imread("photo001.jpg")
+
+winName = "image"
+cv2.namedWindow(winName, cv2.WINDOW_NORMAL)
+cv2.resizeWindow(winName, 900, 600)
+cv2.moveWindow(winName, 100, 100)
+cv2.imshow(winName, img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
